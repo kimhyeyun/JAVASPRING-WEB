cae532c01de5d010cc817d1c289d37b38e21abd3
Kimhyeyun <cqqwer@gmail.com>
2021-06-28 14:07:57 +0900

    pattern

--- JavaSpring/design/src/com/company/design/Main.java
+++ JavaSpring/design/src/com/company/design/Main.java
@@ -1,9 +1,18 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.*;
+import com.company.design.observer.Button;
+import com.company.design.observer.IButtonListener;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
@@ -19,7 +28,7 @@ public class Main {
         System.out.println(aClient.equals(bClient));*/
 
         // Adapter
-        HairDryer hairDryer = new HairDryer();
+       /* HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
         // 220V -> 110V 로 변환 (adapter)
@@ -31,7 +40,76 @@ public class Main {
 
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V airadapter = new SocketAdapter(airConditioner);
-        connect(airadapter);
+        connect(airadapter);*/
+
+
+        //proxy
+       /* Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();*/
+        // 현재 cache 기능이 전혀 없음
+
+        // caching 하는 기
+       /* IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();*/
+
+        // AOP
+
+       /* AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+                );
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());*/
+
+
+        // decorator
+ /*       ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        // a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+
+        // a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+
+        // a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();*/
+
+        // observer
+        Button button = new Button("버튼");
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+        button.click("메세지 전달 : click1");
+        button.click("메세지 전달 : click2");
+        button.click("메세지 전달 : click3");
+        button.click("메세지 전달 : click4");
+
     }
 
 
