c1937dbd7fbcfc5053d3b2b1a0328e82c12c9df1
Kimhyeyun <cqqwer@gmail.com>
2021-07-03 22:52:24 +0900

    RESPONSE

---
+++ JavaSpring/SpringBoot/response/src/main/java/com/example/response/controller/PageController.java
@@ -0,0 +1,34 @@
+package com.example.response.controller;
+
+import com.example.response.dto.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageController {
+
+    // 200 ok
+    // main.html Return
+    @RequestMapping("/main")
+    public String main(){
+        return "main.html";
+    }
+
+
+    // ResponseEntity
+
+    // 사용 x
+    @ResponseBody
+    @GetMapping("/user")
+    public User user(){
+        var user = new User();
+
+        user.setName("steve");
+        user.setAddress("here");
+
+        return user;
+
+    }
+}
