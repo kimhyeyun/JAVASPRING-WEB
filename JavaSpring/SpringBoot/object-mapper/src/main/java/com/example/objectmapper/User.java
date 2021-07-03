edf7414fe17bcb3b161e53464536485df9aff8b7
Kimhyeyun <cqqwer@gmail.com>
2021-07-03 23:25:50 +0900

    모범사례 - Object

---
+++ JavaSpring/SpringBoot/object-mapper/src/main/java/com/example/objectmapper/User.java
@@ -0,0 +1,62 @@
+package com.example.objectmapper;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+
+    private String name;
+    private int age;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    public User(){
+        this.name = null;
+        this.age = 0;
+        this.phoneNumber = null;
+    }
+
+    // get은 넣지 않아야함
+    public User defaultUser(){
+        return new User("default", 0,"default");
+    }
+
+    public User(String name, int age, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
--- JavaSpring/SpringBoot/object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
+++ JavaSpring/SpringBoot/object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.objectmapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ObjectMapperApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("------------");
+
+        // Text JSON -> Object
+        // Object -> Text JSON
+
+        // controller req Json(text) -> object
+        // response object -> Json(text)
+
+        var ObjectMapper = new ObjectMapper();
+
+        // object -> text
+        // object mapper는 get method를 활용
+
+        // error! object getter/setter 만 취급
+        var user = new User("steve",10, "010-1111-2222");
+        var text = ObjectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        // text -> object
+        // object mapper는 default 생성자를 필요로 함
+
+        // error! default 생성자가 필요!
+        var objectUser = ObjectMapper.readValue(text, User.class);
+        System.out.println(objectUser);
     }
 
 }
