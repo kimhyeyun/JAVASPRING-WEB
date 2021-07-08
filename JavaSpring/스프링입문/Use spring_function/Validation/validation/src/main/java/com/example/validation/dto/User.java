22a33feaef7c7d0a0f4d78195825c9cf4984fade
Kimhyeyun <cqqwer@gmail.com>
2021-07-08 23:24:24 +0900

    Custom Validation

--- JavaSpring/스프링입문/Use spring_function/Validation/validation/src/main/java/com/example/validation/dto/User.java
+++ JavaSpring/스프링입문/Use spring_function/Validation/validation/src/main/java/com/example/validation/dto/User.java
@@ -1,12 +1,13 @@
 package com.example.validation.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class User {
-
     @NotBlank
     private String name;
 
@@ -19,6 +20,10 @@ public class User {
     // 정규식
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
     private String phoneNumber;
+
+    @YearMonth
+    @Size(min = 6, max = 6)
+    private String reqYearMonth;    // yyyyMM
 
     public String getName() {
         return name;
@@ -52,6 +57,26 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
+    // 재활용이 불가능함 -> 중복이 발생
+    @AssertTrue(message = "yyyyMM 형식에 맞지 않습니다.")
+    public boolean isReqYearMonthValidation(){
+        try {
+            LocalDate localDate = LocalDate.parse(getReqYearMonth() + "01", DateTimeFormatter.ofPattern("yyyMMdd"));
+        }catch (Exception e){
+            return false;
+        }
+
+        return true;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -59,6 +84,10 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
     }
+
 }
+
+
