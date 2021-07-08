9dbe9aab5d0fa7fb1032b5e08e9757e8a5ac6bb2
Kimhyeyun <cqqwer@gmail.com>
2021-07-08 23:30:19 +0900

    relocate

---
+++ JavaSpring/스프링입문/Use spring_function/Validation/src/main/java/com/example/validation/annotation/YearMonth.java
@@ -0,0 +1,25 @@
+package com.example.validation.annotation;
+
+import com.example.validation.validator.YearMonthValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = {YearMonthValidator.class})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface YearMonth {
+
+    String message() default "yyyyMM 형식에 맞지 않습니다.";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload()default { };
+
+    String pattern() default "yyyyMMdd" +
+            "";
+}
