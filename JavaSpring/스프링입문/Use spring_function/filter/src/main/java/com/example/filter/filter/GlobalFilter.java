b04f8a45de1bf4ae4c28ee132f08ef0a2b3670b6
Kimhyeyun <cqqwer@gmail.com>
2021-07-09 19:47:41 +0900

    FILTER

--- JavaSpring/스프링입문/Use spring_function/filter/src/main/java/com/example/filter/filter/GlobalFilter.java
+++ JavaSpring/스프링입문/Use spring_function/filter/src/main/java/com/example/filter/filter/GlobalFilter.java
@@ -3,32 +3,44 @@ package com.example.filter.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 
 @Slf4j
-@Component
+//@Component
+@WebFilter(urlPatterns = "/api/user/*")
 public class GlobalFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // 전처리
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper httpServletResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
 
-        String url = httpServletRequest.getRequestURI();
-
-        BufferedReader br = httpServletRequest.getReader();
-
-        br.lines().forEach(line -> {
-            log.info("url: {}, line : {}",url, line);
-        });
 
         chain.doFilter(httpServletRequest, httpServletResponse);
+
+        String url = httpServletRequest.getRequestURI();
+        // 후처리
+        // req
+        String reqContent = new String(httpServletRequest.getContentAsByteArray());
+        log.info("request url : {}, request body : {}", url, reqContent);
+
+        //res
+        String resContent = new String(httpServletResponse.getContentAsByteArray());
+        int httpStatus = httpServletResponse.getStatus();
+
+        // 다시 한번 더 바디 내용을 채움
+        httpServletResponse.copyBodyToResponse();
+
+        log.info("response status : {}, response body : {}", httpStatus, resContent);
 
     }
 }
