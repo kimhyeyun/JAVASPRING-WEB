5516311454c7e1be347111b7da1e5f0c1e0ec79f
Kimhyeyun <cqqwer@gmail.com>
2021-07-13 18:10:48 +0900

    ASYNC

---
+++ JavaSpring/스프링입문/Use spring_function/async/src/main/java/com/example/async/controller/ApiController.java
@@ -0,0 +1,24 @@
+package com.example.async.controller;
+
+import com.example.async.service.AsyncService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class ApiController {
+    private AsyncService asyncService;
+
+    @GetMapping("/hello")
+    public CompletableFuture hello(){
+        log.info("completable future init");
+        return asyncService.run();
+    }
+}
