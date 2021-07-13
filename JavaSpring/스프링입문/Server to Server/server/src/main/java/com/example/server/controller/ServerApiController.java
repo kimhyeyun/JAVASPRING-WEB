b80bf6044e4692fe81464e42110515e63d9efb91
Kimhyeyun <cqqwer@gmail.com>
2021-07-13 20:46:40 +0900

    naver api 사용

--- JavaSpring/스프링입문/Server to Server/server/src/main/java/com/example/server/controller/ServerApiController.java
+++ JavaSpring/스프링입문/Server to Server/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,12 +4,47 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URLEncoder;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    @GetMapping("/naver")
+    public String naver(){
+        String query = "갈비집";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query",query)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode().build().toUri();
+
+        log.info("uri : {}",uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","BKSHZ_NPUv4DB3YH_dNz")
+                .header("X-Naver-Client-Secret", "wPxQlzzAue")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age){
