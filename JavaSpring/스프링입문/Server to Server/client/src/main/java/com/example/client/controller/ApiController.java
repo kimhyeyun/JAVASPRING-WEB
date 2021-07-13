19e71da9e5d3a4d849f3a4583a1e58e106e843b5
Kimhyeyun <cqqwer@gmail.com>
2021-07-13 19:18:05 +0900

    CLIENT

---
+++ JavaSpring/스프링입문/Server to Server/client/src/main/java/com/example/client/controller/ApiController.java
@@ -0,0 +1,21 @@
+package com.example.client.controller;
+
+import com.example.client.dto.UserResponse;
+import com.example.client.service.RestTemplateService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+@AllArgsConstructor
+public class ApiController {
+
+    private RestTemplateService restTemplateService;
+
+    @GetMapping("/hello")
+    public UserResponse getHello(){
+        return restTemplateService.hello();
+    }
+}
---
+++ JavaSpring/스프링입문/Server to Server/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,43 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class RestTemplateService {
+
+    // http://localhost:9090/api/server/hello 요청해서
+    // response 받아옴
+
+    public UserResponse hello(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .queryParam("name", "steve")
+                .queryParam("age", 10)
+                .encode()
+                .build()
+                .toUri();
+
+        log.info("uri : {}", uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+//        String result = restTemplate.getForObject(uri, String.class);
+//        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri,UserResponse.class);
+
+
+        log.info("status code : {}", result.getStatusCode());
+        log.info("body : {}", result.getBody());
+
+        return result.getBody();
+    }
+}
