c55dcee4218b5b42fdff2ddebb5ec1e75055cd3c
Kimhyeyun <cqqwer@gmail.com>
2022-01-13 23:17:28 +0900

    01/13 - 스프링 컨테이너 / DI 컨테이너

--- inflearn/core/src/main/java/hello/core/AppConfig.java
+++ inflearn/core/src/main/java/hello/core/AppConfig.java
@@ -8,12 +8,17 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
@@ -21,10 +26,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
